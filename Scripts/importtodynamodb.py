32f86e204aed0e95a4c22373098ffae79e3d611c
markoaular <markoaular@gmail.com>
2019-08-25 21:37:16 -0300

    Add files via upload

---
+++ Scripts/importtodynamodb.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import boto3
+import json
+import decimal
+import os 
+ 
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1, endpoint=localhost:4569')
+ 
+table = dynamodb.Table('talks')
+ 
+rootdir = "d:\\projects\\image-annotation\\data\\talks\\json\\1"
+for subdir, dirs, files in os.walk(rootdir):
+  for file in files:
+    selected = subdir + "\\" + file
+    try:
+      with open(selected, encoding="UTF-8") as json_file:
+        row = json.load(json_file, parse_float = decimal.Decimal)
+        row['id_i'] = str(row['id_i'])
+ 
+        #print(row)
+ 
+        table.put_item(
+          Item=row
+        )
+    except:
+      print("error: " + selected)
